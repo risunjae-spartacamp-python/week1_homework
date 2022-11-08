79c153815330103b6fb66e3f01f463f8c9e3650e
risunjae121 <risunjae121@yahoo.co.jp>
2022-11-08 23:58:56 +0900

    add work1_RPA

---
+++ work1_RPA.py
@@ -0,0 +1,22 @@
+import pyautogui
+import webbrowser
+import time
+import pyperclip
+
+# while True:
+#     x, y = pyautogui.position()
+#     positionStr = "X: " + str(x).rjust(4) + " Y: " + str(y).rjust(4)
+#     print(positionStr, end="")
+#     print("\b" * len(positionStr), end="", flush=True)
+webbrowser.open("https://www.google.com/")
+pyautogui.click(x=520, y=-290)
+time.sleep(0.5)
+pyperclip.copy("平泉町　観光")
+pyautogui.hotkey("command", "v")
+time.sleep(0.5)
+pyautogui.press("enter")
+time.sleep(1)
+pyautogui.screenshot("screenshot.png")
+time.sleep(1)
+pyautogui.save("screenshot.png")
+time.sleep(1)
